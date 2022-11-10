a778cb336f16c27477e3eabbba45775e51f7e800
Jw95z <michaelmichael6909@gmail.com>
2022-11-09 16:40:36 -0800

    Changes to be committed: 	new file:   templates/updates.html 	modified:   update.py

--- update.py
+++ update.py
@@ -5,10 +5,10 @@ from flask_restful import Api, Resource
 update = Blueprint('update', __name__,
                    url_prefix='/update')
 
-
 api = Api(update)
 
 def keepUpdating():
+    global response
     url = "https://motivational-quotes1.p.rapidapi.com/motivation"
 
     payload = {
@@ -21,7 +21,27 @@ def keepUpdating():
 	    "X-RapidAPI-Host": "motivational-quotes1.p.rapidapi.com"
     }
 
-    response = requests.request("POST", url, json=payload, headers=headers)
-    print(response.text)
+    response = requests.request("GET", url, json=payload, headers=headers)
+    print(response)
     threading.Timer(2.0, keepUpdating).start()
-threading.Timer(2.0, keepUpdating).start()
+
+
+
+
+"""Defines API Resources 
+  URLs are defined with api.add_resource
+"""   
+
+
+
+"""Main or Tester Condition 
+  This code only runs when this file is played directly
+"""        
+if __name__ == "__main__": 
+    
+    # This code looks for "world data"
+    threading.Timer(2.0, keepUpdating).start()
+    
+     # turn response to json() so we can extract "world_total"
+
+    # This code looks for USA in "countries_stats"
